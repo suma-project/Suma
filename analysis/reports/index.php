766cc69276cd56a0c29e9cbb7643e2f1a0a28bee
Bret Davidson <bret_davidson@ncsu.edu>
2012-08-20 13:06:55 -0400

    Added Twitter Bootstrap styles to index page

--- analysis/reports/index.php
+++ analysis/reports/index.php
@@ -1,7 +1,58 @@
-<html>
-<body>
-    <a href="location_bar">Location Bar Chart</a><br />
-    <a href="activity_pie">Activity Pie Chart</a><br />
-    <a href="line_chart">Line Chart</a>
-</body>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Suma Reports</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link href="../lib/css/bootstrap.css" rel="stylesheet">
+    <style>
+      body {
+        padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+      }
+    </style>
+    <link href="../lib/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-fixed-top">
+        <div class="navbar-inner">
+            <div class="container">
+                <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </a>
+                <a class="brand" href="#">Suma Reports</a>
+                <div class="nav-collapse">
+                    <ul class="nav">
+                        <li class="active"><a href="#">Home</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+
+        <h1>Available Reports</h1>
+      
+        <a href="line_chart_d3">Line Chart with d3.js</a><br />
+        <a href="calendar">Calendar</a><br />
+
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <script src="../lib/js/bootstrap.min.js"></script>
+
+  </body>
 </html>
