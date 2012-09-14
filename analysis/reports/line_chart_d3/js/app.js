77ff3e21418563889dd863a24db300e465166cd7
Bret Davidson <bret_davidson@ncsu.edu>
2012-09-14 14:54:40 -0400

    Refactored code, improved error handling and interface behavior, improved module instantiation, added dependency injection.

--- analysis/reports/line_chart_d3/js/app.js
+++ analysis/reports/line_chart_d3/js/app.js
@@ -1,8 +1,34 @@
-(function () {
-    var Chart = {
-        // Initializes application
+/**
+ * Central application for chart display.
+ *
+ * @author  Bret Davidson <bret_davidson@ncsu.edu>
+ *
+ * @property {object} filters Instantiated filter module
+ * @property {object} chart Instantiated time series chart
+ */
+(function (ReportFilters, TimeSeries) {
+    var App = {
+        filters: undefined,
+        chart: undefined,
+        /**
+         * Initializes app
+         * 
+         * @this {App}
+         */
         init: function () {
+            // Insert default dates
+            this.insertDefaultDates();
 
+            // Insert filter select boxes
+            this.insertFilters();
+
+            // Bind events (AJAX Call)
+            this.bindEvents();
+        },
+        /**
+         * Inserts default dates into form
+         */
+        insertDefaultDates : function () {
             // Create dates for default date display
             var now = moment().format('YYYY-MM-DD'),
                 then = moment().subtract('months', 6).format('YYYY-MM-DD');
@@ -10,59 +36,114 @@
             // Insert default dates into DOM
             $('#sdate').val(then);
             $('#edate').val(now);
+        },
+        /**
+         * Initializes and inserts secondary filters
+         */
+        insertFilters: function () {
+            // Create options object for filters
+            var filterOptions = {
+                url: '../line_chart_d3/reportFilters.php',
+                triggerForm: '#initiatives',
+                filterForm: '#secondary-filters',
+                locationsTemplate: '#locations-template',
+                activitiesTemplate: '#activities-template',
+                locationsSelect: '#locations',
+                activitiesSelect: '#activities'
+            };
+                // Initialize filters
+            if (this.filters === undefined) {
+                this.filters = new ReportFilters(filterOptions);
+            }
+
+            this.filters.init();
+        },
+        /**
+         * Binds events for datepicker and data AJAX call.
+         */
+        bindEvents: function () {
+            var self = this;
 
             // Initialize datepicker
             $('#sdate').datepicker({'format': 'yyyy-mm-dd', 'autoclose': 'true'});
             $('#edate').datepicker({'format': 'yyyy-mm-dd', 'autoclose': 'true'});
 
-            // Event handler to initiate application
+            // Event handler to initialize AJAX call
             $('body').on('submit', '#chartFilters', function (e) {
                 var input = $(this).serializeArray();
 
-                $.when(Chart.getData(input)).then(function (data) {
-                    Chart.processData(data);
-                });
+                $.when(self.getData(input))
+                    .then(function (data) {
+                        var counts = self.processData(data);
+
+                        if (counts) {
+                            self.drawChart(counts);
+                        } else {
+                            self.noData();
+                        }
+                    }, function (e) {
+                        $('#ajax-error').show();
+                    });
 
                 e.preventDefault();
             });
         },
-
-        // AJAX call to query server, accepts form input as parameter
+        /**
+         * AJAX call to retrieve data
+         * 
+         * @param  {array} input
+         * @return {object} Returns a jQuery promise object
+         */
         getData: function (input) {
             return $.ajax({
                 url: 'results.php',
                 data: input,
                 dataType: 'json',
+                // These are a hot mess and need to be optimized
                 beforeSend: function () {
-                    $('svg').remove();
-                    $('.alert').hide();
-                    $('#loading').show();
                     var text = $('#submit').data('loading-text');
                     $('#submit').addClass('disabled').val(text);
                     $('#submit').attr('disabled', 'true');
+                    $('svg').remove();
+                    $('.alert').hide();
+                    $('#loading').show();
                 },
                 complete: function () {
                     var text = $('#submit').data('default-text');
                     $('#submit').removeClass('disabled').val(text);
-                    $('#loading').hide();
                     $('#submit').removeAttr('disabled');
+                    $('#loading').hide();
                 }
             });
         },
-
-        // Sort data according to date
+        /**
+         * Sort ascending according to date, meant to be used with
+         * native arr.sort() method.
+         * 
+         * @param  {object} a
+         * @param  {object} b
+         * @return {integer}
+         */
         sortData: function (a, b) {
             return new Date(a.date).getTime() - new Date(b.date).getTime();
         },
-
-        showError: function () {
-            $('.alert-error').show();
+        /**
+         * Display error message
+         */
+        noData: function () {
+            $('#no-data').show();
             $('#submit').removeAttr('disabled');
         },
-
-        // Process and prepare data for display, accepts response from getData
+        /**
+         * Process response from AJAX call
+         * 
+         * @param  {object} response
+         * @return {array}
+         */
         processData: function (response) {
-            var counts = [];
+            var self = this,
+                counts = [],
+                testLength;
 
             _.each(response, function (element, index) {
                 var newObj = {
@@ -74,28 +155,36 @@
                 counts.push(newObj);
             });
 
-            var test = _.unique(_.pluck(_.values(counts), 'count'));
-            if(test.length === 1) {
-                Chart.showError();
-                return;
+            // Check if counts is large enough to display meaningfully
+            testLength = _.unique(_.pluck(_.values(counts), 'count'));
+
+            if (testLength.length === 1) {
+                return false;
             }
+
             // Sort by date
-            counts.sort(Chart.sortData);
+            counts.sort(self.sortData);
 
-            Chart.drawChart(counts);
+            return counts;
         },
-
+        /**
+         * Draw chart
+         * 
+         * @param  {array} counts
+         */
         drawChart: function (counts) {
-            var chart = new timeSeries();
+            if (!this.chart) {
+                this.chart = new TimeSeries();
+            }
 
             d3.select("#chart")
                 .datum(counts)
-                .call(chart);
+                .call(this.chart);
         }
     };
 
     // Start app on document ready
     $(document).ready(function () {
-        Chart.init();
+        App.init();
     });
-}());
+}(ReportFilters, TimeSeries));
