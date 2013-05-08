8476d91170d0c51e675526d6d20b3e7a15a37de1
Bret Davidson <bret_davidson@ncsu.edu>
2013-05-08 16:58:05 -0400

    example config file

--- analysis/reports/nightly/nightly_config_example.php
+++ analysis/reports/nightly/nightly_config_example.php
@@ -6,6 +6,6 @@ date_default_timezone_set($DEFAULT_TIMEZONE);
 
 $DAY_DISPLAY = date("m-d-Y", strtotime("yesterday"));
 $DAY_PROCESS = date("Ymd", strtotime("yesterday"));
-$RECIPIENTS  = "bddavids@ncsu.edu";
+$RECIPIENTS  = "EMAIL ADDRESSES SEPARATED BY COMMA";
 $GREETING    = "Below are the hourly Suma counts for " . $DAY_DISPLAY . ".";
 $SUBJECT     = "Suma Nightly Report: " . $DAY_DISPLAY;
