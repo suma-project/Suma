d549ba0331cca5735f92e2dcb6f7310348649d02
Jason Casden <jmcasden@ncsu.edu>
2013-07-11 11:36:11 -0400

    Add an Activity to new Activity Groups by default

--- service/web/js/views/admin/initiatives.js
+++ service/web/js/views/admin/initiatives.js
@@ -337,10 +337,11 @@ $(document).ready(function(){
     });
 
     $("body").on("click", "#addActivityGroup", function() {
-        $('#activities').prepend('<li class="activityGroup allowMulti-act-group"><div><span class="actGroupTitle">New Activity Group</span>' +
+        $('<li class="activityGroup allowMulti-act-group"><div><span class="actGroupTitle">New Activity Group</span>' +
             '<span class="actGroupDesc"></span><span class="actGroupID">new-act-group</span><span class=\"activityControls\">' +
-            '<a href=\"#\" class=\"addActivity\">Add Activity</a><a href="#" class="editActGroup">Edit</a></span></div><ol></ol></li>');
+            '<a href=\"#\" class=\"addActivity\">Add Activity</a><a href="#" class="editActGroup">Edit</a></span></div><ol></ol></li>').prependTo('#activities').find('a.addActivity').click();
         $('#activities').nestedSortable('refresh');
+
         return false;
     });
 
