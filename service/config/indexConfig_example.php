378160a5412e2c066705514d3d4df56cf73a4ce2
Bret Davidson <bret_davidson@ncsu.edu>
2013-07-15 13:50:35 -0400

    Revert "moved logic out of indexConfig"
    This reverts commit 25deeebe116966acca6f996aa809561ec87f60a8.

--- service/config/indexConfig_example.php
+++ service/config/indexConfig_example.php
@@ -7,3 +7,24 @@ $SUMA_BASE_URL = '/sumaserver';
 
 // Debug mode
 $SUMA_DEBUG = false;
+
+// Debug Mode Setup
+if ($SUMA_DEBUG == false)
+{
+    $SUMA_ERROR_REPORTING  = 0;
+    $SUMA_DISPLAY_ERRORS   = 'off';
+    $SUMA_THROW_EXCEPTIONS =  false;
+}
+else
+{
+    $SUMA_ERROR_REPORTING  = E_ERROR | E_WARNING | E_PARSE | E_NOTICE;
+    $SUMA_DISPLAY_ERRORS   = 'on';
+    $SUMA_THROW_EXCEPTIONS =  true;
+}
+
+// Error Reporting
+error_reporting($SUMA_ERROR_REPORTING);
+ini_set('display_errors', $SUMA_DISPLAY_ERRORS);
+
+// Set paths
+ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . $SUMA_SERVER_PATH . PATH_SEPARATOR . $SUMA_SERVER_PATH . '/lib/zend/library');
