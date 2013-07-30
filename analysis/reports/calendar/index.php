19d5861e4131e1ea4b4ba333617f0f2e1e2f26c7
Bret Davidson <bret_davidson@ncsu.edu>
2013-07-30 11:41:57 -0400

    add filters to calendar interface

--- analysis/reports/calendar/index.php
+++ analysis/reports/calendar/index.php
@@ -66,18 +66,38 @@ $initDropDown .= '</select>';
         </div>
 
         <div class="container">
-            <form>
-                <div class="control-group">
-                    <h5 class="suma-popover" rel="popover" data-trigger="hover" data-delay="300" data-title="Choose Initiative" data-content="Choose an initiative to reveal additional filters.">Choose Initiative</h5>
-                        <label class="control-label" for="initiatives"></label>
-                        <div class="controls">
-                            <?php echo $initDropDown; ?>
-                        </div>
+            <!-- <div id="main-chart-header" class="row">
+                <div id="main-annotation" class="span7">
+
                 </div>
-                <div>
-                    <input type="submit" id="submit" class="btn btn-success" data-default-text ="Submit" data-loading-text="Loading..." value="Submit" />
+                <div class="btn-toolbar pull-right">
+                    <div id="main-chart-avgsum" class="btn-group" data-toggle="buttons-radio">
+                        <button type="button" class="btn btn-small" value="avg">Daily Avg</button>
+                        <button type="button" class="btn btn-small active" value="sum">Daily Sum</button>
+                    </div>
+                    <div class="btn-group">
+                        <a class="btn btn-small" href="#summary-data">Summary Data</a>
+                    </div>
+                    <div class="btn-group">
+                        <a class="btn btn-small" href="#csv-export">Export</a>
+                    </div>
+                    <div id="main-chart-download" class="btn-group">
+                        <a id="main-download" download="suma_main_chart.png" data-chart-div="chart1" class="btn btn-small" target="_blank">Save Chart</a>
+                    </div>
                 </div>
-            </form>
+            </div> -->
+            <div id="no-data" class="alert alert-error alert-block">
+                <h4>Warning!</h4>
+                    There was insufficient data for display based on your search parameters. Please try a different combination of filters.
+            </div>
+            <div id="ajax-error" class="alert alert-error alert-block">
+                <h4>Warning!</h4>
+                    There was a problem retrieving data from the server. It is possible no data exists for that combination of filters. Please try again or contact your system administrator.
+            </div>
+            <div id ="welcome" class="alert alert-info alert-block">
+                <h4>Welcome!</h4>
+                    Please select an initiative from the select menu below. Once you have chosen an initiative, additional filter options will appear. You can also limit your search by date or time.
+            </div>
             <div id="chart">
                 <div id="loading"><img src="../../lib/img/spinner.gif"></div>
             </div>
@@ -92,14 +112,154 @@ $initDropDown .= '</select>';
                 </ul>
                 <span>More</span>
             </div>
+            <div class="span12">
+                <div class="row">
+                    <form id="chartFilters">
+                        <fieldset>
+                            <div class="span9">
+                                <div class="row">
+                                <div class="span9">
+                                    <h3>Modify Chart</h3>
+                                </div>
+                                <div class="span3">
+                                    <div class="control-group">
+                                        <h5 class="suma-popover" rel="popover" data-trigger="hover" data-delay="300" data-title="Choose Initiative" data-content="Choose an initiative to reveal additional filters.">Choose Initiative</h5>
+                                        <label class="control-label" for="initiatives"></label>
+                                        <div class="controls">
+                                            <?php echo $initDropDown; ?>
+                                        </div>
+                                    </div>
+                                    <div class="control-group">
+                                        <h5 class="suma-popover" rel="popover" data-trigger="hover" data-delay="300" data-title="Classify Counts By" data-content='Choose the date to use for grouping counts by date. "Count Date" shows the actual date of the count, while "Session Start" and "Session End" use the respective date on the collection session containing each count.'>Classify Counts By</h5>
+                                        <label class="control-label" for="session"></label>
+                                        <div class="controls">
+                                             <select name="session" id="session">
+                                                <option value="count">Count Date</option>
+                                                <option value="start">Session Start</option>
+                                                <option value="end">Session End</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="control-group">
+                                        <h5 class="suma-popover" rel="popover" data-trigger="hover" data-delay="300" data-title="Always Include Whole Session" data-content='Select yes if you would like to include counts inside of sessions that may fall outside of your other filters.'>Always Include Whole Session</h5>
+                                        <label class="control-label" for="session_filter"></label>
+                                        <div class="controls">
+                                             <select name="session_filter" id="session_filter">
+                                                <option value="false">No</option>
+                                                <option value="true">Yes</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="span3">
+                                    <div class="control-group">
+                                        <h5 class="suma-popover" rel="popover" data-trigger="hover" data-delay="300" data-title="Choose Date Range" data-content="Choose a start and end date for your analysis. Defaults to 6 months from current day. Clear fields to retrieve the complete data set.">Choose Date Range</h5>
+                                        <label class="control-label" for="sdate">Start Date</label>
+                                        <div class="controls">
+                                            <input type="text" id="sdate" name="sdate" class="input-medium" />
+                                        </div>
+                                    </div>
+                                    <div class="control-group">
+                                        <label class="control-label" for="edate">End Date</label>
+                                        <div class="controls">
+                                            <input type="text" id="edate" name="edate" class="input-medium"/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="span3">
+                                    <div class="control-group">
+                                        <h5 class="suma-popover" rel="popover" data-trigger="hover" data-delay="300" data-title="Choose subset of daily data" data-content="Include only data gathered during a certain time of day in your analysis, e.g. 8pm-12am.">Choose subset of daily data</h5>
+                                        <label class="control-label" for="stime">Start Time</label>
+                                        <div class="controls">
+                                            <input type="text" id="stime" name="stime" placeholder="00:00" class="input-medium"/>
+                                            <span class="help-block">24-hour format, e.g. 08:00</span>
+                                        </div>
+                                    </div>
+                                    <div class="control-group">
+                                        <label class="control-label" for="etime">End Time</label>
+                                        <div class="controls">
+                                            <input type="text" id="etime" name="etime" placeholder="24:00" class="input-medium"/>
+                                            <span class="help-block">24-hour format, e.g. 08:00</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                <div id="secondary-loading" class="span3"><img id="secondary-spinner" src="../../lib/img/spinner.gif"></div>
+                <div id="secondary-filters" class="span3">
+                    <h3>Initiative Filters</h3>
+                      <div class="control-group">
+                        <h5 class="suma-popover" rel="popover" data-trigger="hover" data-delay="300" data-title="Limit days of the week" data-content="Filter days by weekday or weekend.">Limit days of the week</h5>
+                        <label class="control-label" for="daygroup"></label>
+                        <div class="controls">
+                            <select name="daygroup" id="daygroup">
+                                <option value="all">All</option>
+                                <option value="weekdays">Weekdays Only</option>
+                                <option value="weekends">Weekends Only</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <h5 class="suma-popover" rel="popover" data-trigger="hover" data-delay="300" data-title="Limit Locations" data-content="Select which locations to include in your analysis. Selecting a location with children will include all children in the data set.">Limit locations</h5>
+                        <label class="control-label" for="locations"></label>
+                        <div class="controls">
+                            <select name="locations" id="locations">
+                            </select>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <h5 class="suma-popover" rel="popover" data-trigger="hover" data-delay="300" data-title="Limit Activities" data-content="Select which activities to include in your analysis. Activity Groups will include all member activities.">Limit activities</h5>
+                        <label class="control-label" for="activities"></label>
+                        <div class="controls">
+                            <select name="activities" id="activities">
+                            </select>
+                        </div>
+                    </div>
+                     <div>
+                        <input type="submit" id="submit" class="btn btn-success" data-default-text ="Submit" data-loading-text="Loading..." value="Submit" />
+                    </div>
+                </div>
+                </fieldset>
+                </form>
+            </div>
+            </div>
+            <!-- <form>
+                <div class="control-group">
+                    <h5 class="suma-popover" rel="popover" data-trigger="hover" data-delay="300" data-title="Choose Initiative" data-content="Choose an initiative to reveal additional filters.">Choose Initiative</h5>
+                        <label class="control-label" for="initiatives"></label>
+                        <div class="controls">
+                            <?php echo $initDropDown; ?>
+                        </div>
+                </div>
+                <div>
+                    <input type="submit" id="submit" class="btn btn-success" data-default-text ="Submit" data-loading-text="Loading..." value="Submit" />
+                </div>
+            </form> -->
+
         </div>
+
+        <!-- Templates -->
+        <script id="locations-template" type="text/x-handlebars-template">
+            <option value="all">All</option>
+            {{#each items}}
+                <option value="{{id}}">{{indent depth}}{{title}}</option>
+            {{/each}}
+        </script>
+        <script id="activities-template" type="text/x-handlebars-template">
+            <option value="all">All</option>
+            {{#each items}}
+                <option value="{{type}}-{{id}}">{{indent depth}}{{title}}</option>
+            {{/each}}
+        </script>
 
         <script src="../../lib/js/jquery.min.js"></script>
         <script src="../../lib/js/bootstrap.min.js"></script>
         <script src="../../lib/js/bootstrap-datepicker.js"></script>
+        <script src="../../lib/js/handlebars.js"></script>
         <script src="../../lib/js/d3.v3.min.js"></script>
         <script src="../../lib/js/lodash.min.js"></script>
         <script src="../../lib/js/moment.js"></script>
+        <script src="../../lib/js/ReportFilters.js"></script>
         <script src="../../lib/js/Calendar.js"></script>
         <script src="js/app.js"></script>
 
--- analysis/reports/calendar/js/app.js
+++ analysis/reports/calendar/js/app.js
@@ -1,14 +1,64 @@
 (function (Calendar) {
     var App = {
+        filters: null,
         init: function () {
+            // Insert default dates
+            this.insertDefaultDates();
+
+            // Insert filter select boxes
+            this.insertFilters();
+
+            // Set initiative filter to default (for back button)
+            $('#initiatives').val('default');
+
+            // Bind Events
             this.bindEvents();
+        },
+        /**
+         * Inserts default dates into form
+         */
+        insertDefaultDates : function () {
+            // Create dates for default date display
+            var now = moment().format('YYYY-MM-DD'),
+                then = moment().subtract('months', 6).format('YYYY-MM-DD');
+
+            // Insert default dates into DOM
+            $('#sdate').val(then);
+            $('#edate').val(now);
+        },
+        /**
+         * Initializes and inserts secondary filters
+         */
+        insertFilters: function () {
+            // Create options object for filters
+            var filterOptions = {
+                url: '../../lib/php/reportFilters.php',
+                triggerForm: '#initiatives',
+                filterForm: '#secondary-filters',
+                locationsTemplate: '#locations-template',
+                activitiesTemplate: '#activities-template',
+                locationsSelect: '#locations',
+                activitiesSelect: '#activities'
+            };
+
+            // Initialize filters
+            if (this.filters === null) {
+                this.filters = new ReportFilters(filterOptions);
+            }
+
+            this.filters.init();
         },
         bindEvents: function () {
             var self = this;
 
+            // Initialize datepicker
+            $('#sdate').datepicker({'format': 'yyyy-mm-dd', 'autoclose': 'true'});
+            $('#edate').datepicker({'format': 'yyyy-mm-dd', 'autoclose': 'true'});
+
+            // Get chart data on submit
             $('body').on('submit', 'form', function (e) {
                 var input = $(this).serializeArray();
-
+                console.log('input', input)
                 $.when(self.getData(input))
                     .then(self.processData.bind(self))
                     .then(self.drawChart, self.error);
@@ -25,6 +75,7 @@
                 beforeSend: function () {
                     $('#loading').show();
                     $("#legend").hide();
+                    $('#welcome').hide();
                     $('svg').remove();
                 },
                 success: function () {
