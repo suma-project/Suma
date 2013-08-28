077e567265d15f9d4878cce37f150677cbb98833
Bret Davidson <bret_davidson@ncsu.edu>
2013-08-28 10:17:25 -0400

    simplify navbar markup on fixed layout

--- analysis/reports/time-series/index.php
+++ analysis/reports/time-series/index.php
@@ -46,22 +46,12 @@ $initDropDown .= '</select>';
     <body>
         <div class="navbar navbar-default navbar-fixed-top">
             <div class="container">
-                <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                  <span class="sr-only">Toggle navigation</span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                </button>
-                    <a class="navbar-brand" href=".."><img src="../../lib/img/logo.png"></a>
-                </div>
-                <div class="collapse navbar-collapse bs-navbar-collapse">
-                    <ul class="nav navbar-nav">
-                        <li><a href="..">Home</a></li>
-                        <li><a href="../about.html">About</a></li>
-                        <li><a href="../contact.html">Contact</a></li>
-                    </ul>
-                </div>
+                <a class="navbar-brand" href=".."><img src="../../lib/img/logo.png"></a>
+                <ul class="nav navbar-nav">
+                    <li><a href="..">Home</a></li>
+                    <li><a href="../about.html">About</a></li>
+                    <li><a href="../contact.html">Contact</a></li>
+                </ul>
             </div>
         </div>
 
