44690ae68177310612bca3ade97bbebd481f4f1a
Bret Davidson <bret_davidson@ncsu.edu>
2014-01-13 14:16:49 -0500

    update barChart tests

--- analysis/karma.conf.js
+++ analysis/karma.conf.js
@@ -36,6 +36,7 @@ module.exports = function(config) {
       'test/js/spec/directives/chartDownload.js',
       'test/js/spec/directives/csv.js',
       'test/js/spec/directives/csvHourly.js',
+      'test/js/spec/directives/barChart.js',
       // 'test/js/spec/**/*.js',
       'src/views/directives/**/*.html'
     ],
--- analysis/test/js/spec/directives/barChart.js
+++ analysis/test/js/spec/directives/barChart.js
@@ -6,16 +6,35 @@ describe('Directive: barChart', function () {
   beforeEach(module('sumaAnalysis'));
 
   var element,
-    scope;
+      scope,
+      linkScope,
+      stub;
 
-  beforeEach(inject(function ($rootScope) {
+  beforeEach(inject(function ($rootScope, $compile) {
+    element = angular.element('<bar-chart id="chart-2" data="data.barChartData"></bar-chart>');
+
     scope = $rootScope.$new();
+
+    element = $compile(element)(scope);
+    scope.$digest();
+
+    linkScope = element.isolateScope();
   }));
 
   it('should make hidden element visible', inject(function ($compile) {
-    // element = angular.element('<bar-chart></bar-chart>');
-    // element = $compile(element)(scope);
-    // expect(element.text()).toBe('this is the barChart directive');
-    expect(true).to.equal(false);
+    stub = sinon.stub(linkScope, 'render');
+    stub.returns(true);
+
+    scope.$apply(function() {
+      scope.data = {};
+      scope.data.barChartData = [1, 2, 3, 4];
+    });
+
+    scope.$apply(function() {
+      scope.data.barChartData = [5, 6, 7, 8];
+    });
+
+    expect(stub).to.be.calledTwice();
+    stub.restore();
   }));
 });
