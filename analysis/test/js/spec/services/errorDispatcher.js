23243a9615cd977f59c37c140cd771f51d9618af
Bret Davidson <bret_davidson@ncsu.edu>
2014-01-13 11:20:31 -0500

    update errorDispatcher tests

--- analysis/test/js/spec/services/errorDispatcher.js
+++ analysis/test/js/spec/services/errorDispatcher.js
@@ -6,13 +6,38 @@ describe('Service: Errordispatcher', function () {
   beforeEach(module('sumaAnalysis'));
 
   // instantiate service
-  var Errordispatcher;
-  beforeEach(inject(function (_errorDispatcher_) {
+  var Errordispatcher,
+      Rootscope,
+      Uistates,
+      data,
+      stub;
+
+  beforeEach(inject(function (_errorDispatcher_, _uiStates_, _$rootScope_) {
     Errordispatcher = _errorDispatcher_;
+    Rootscope = _$rootScope_,
+    Uistates = _uiStates_;
+
+    data = {message: 'Hello', code: 401};
+
+    stub = sinon.stub(Uistates, 'setUIState');
+    stub.returns(true);
   }));
 
-  it('should do something', function () {
-    expect(!!Errordispatcher).to.be(true);
+  it('should update state via Uistates', function () {
+    Errordispatcher.dispatch(data, Rootscope);
+
+    expect(Uistates.setUIState).to.be.calledOnce();
+
+    stub.restore();
+  });
+
+  it('should attach error message and code to scope', function () {
+    Errordispatcher.dispatch(data, Rootscope);
+
+    expect(Rootscope.errorMessage).to.equal(data.message);
+    expect(Rootscope.errorCode).to.equal(data.code);
+
+    stub.restore();
   });
 
 });
