9eb79e2a82fd081bb5e23c5b9c9b99516fb817cc
Bret Davidson <bret_davidson@ncsu.edu>
2014-01-13 14:41:37 -0500

    update timeSeriesChart tests

--- analysis/test/js/spec/directives/timeSeriesChart.js
+++ analysis/test/js/spec/directives/timeSeriesChart.js
@@ -1,21 +1,40 @@
 'use strict';
 
 describe('Directive: timeSeriesChart', function () {
-
   // load the directive's module
-  beforeEach(module('sumaAnalysis'));
+    beforeEach(module('sumaAnalysis'));
 
-  var element,
-    scope;
+    var element,
+        scope,
+        linkScope,
+        renderStub;
 
-  beforeEach(inject(function ($rootScope) {
-    scope = $rootScope.$new();
-  }));
+    beforeEach(inject(function ($rootScope, $compile) {
+      element = angular.element('<time-series-chart id="chart-1" data="data.timeSeriesData" ng-show="success"></time-series-chart>');
 
-  it('should make hidden element visible', inject(function ($compile) {
-    // element = angular.element('<timeseries-chart></timeseries-chart>');
-    // element = $compile(element)(scope);
-    // expect(element.text()).toBe('this is the timeseriesChart directive');
-    expect(true).to.equal(false)
-  }));
+      scope = $rootScope.$new();
+
+      element = $compile(element)(scope);
+      scope.$digest();
+
+      linkScope = element.isolateScope();
+    }));
+
+    it('should respond to data change and call render', inject(function ($compile) {
+      renderStub = sinon.stub(linkScope, 'render');
+      renderStub.returns(true);
+
+      scope.$apply(function() {
+        scope.data = {};
+        scope.data.timeSeriesData = [1, 2, 3, 4];
+      });
+
+      scope.$apply(function() {
+        scope.data.timeSeriesData = [5, 6, 7, 8];
+      });
+
+      expect(renderStub).to.be.calledTwice();
+
+      renderStub.restore();
+    }));
 });
