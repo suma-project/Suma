e1c4632d53195bcaaa537eae1e819048315bf937
Bret Davidson <bret_davidson@ncsu.edu>
2014-01-13 14:24:31 -0500

    update calendarChart tests

--- analysis/karma.conf.js
+++ analysis/karma.conf.js
@@ -37,6 +37,7 @@ module.exports = function(config) {
       'test/js/spec/directives/csv.js',
       'test/js/spec/directives/csvHourly.js',
       'test/js/spec/directives/barChart.js',
+      'test/js/spec/directives/calendarChart.js',
       // 'test/js/spec/**/*.js',
       'src/views/directives/**/*.html'
     ],
--- analysis/test/js/spec/directives/calendarChart.js
+++ analysis/test/js/spec/directives/calendarChart.js
@@ -6,15 +6,42 @@ describe('Directive: calendarChart', function () {
   beforeEach(module('sumaAnalysis'));
 
   var element,
-    scope;
+      scope,
+      linkScope,
+      renderStub,
+      statsStub;
 
-  beforeEach(inject(function ($rootScope) {
+  beforeEach(inject(function ($rootScope, $compile) {
+    element = angular.element('<calendar-chart id="chart-1" data="data.data" stats="stats" ng-show="success"></calendar-chart>');
+
     scope = $rootScope.$new();
+
+    element = $compile(element)(scope);
+    scope.$digest();
+
+    linkScope = element.isolateScope();
   }));
 
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<calendar-chart></calendar-chart>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the calendarChart directive');
+  it('should respond to data change and call render', inject(function ($compile) {
+    renderStub = sinon.stub(linkScope, 'render');
+    renderStub.returns(true);
+
+    statsStub = sinon.stub(linkScope, 'updateStats');
+    statsStub.returns(true);
+
+    scope.$apply(function() {
+      scope.data = {};
+      scope.data.data = [1, 2, 3, 4];
+    });
+
+    scope.$apply(function() {
+      scope.data.data = [5, 6, 7, 8];
+    });
+
+    expect(renderStub).to.be.calledTwice();
+    expect(statsStub).to.be.calledTwice();
+
+    renderStub.restore();
+    statsStub.restore();
   }));
 });
