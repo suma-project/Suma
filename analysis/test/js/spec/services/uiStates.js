7c3f2ad31ae58c4b3b04462ee4221db2b32c23d2
Bret Davidson <bret_davidson@ncsu.edu>
2014-01-13 11:07:58 -0500

    update uiStates test

--- analysis/test/js/spec/services/uiStates.js
+++ analysis/test/js/spec/services/uiStates.js
@@ -8,14 +8,42 @@ describe('Service: Uistates', function () {
   // instantiate service
   var Uistates,
       Rootscope;
+
   beforeEach(inject(function (_uiStates_, _$rootScope_) {
     Uistates = _uiStates_;
     Rootscope = _$rootScope_;
   }));
 
-  it('should update state', function () {
+  it('should update initial to true', function () {
     Uistates.setUIState('initial', Rootscope);
-    expect(Rootscope.state).to.equal(undefined);
+    expect(Rootscope['initial']).to.equal(true);
+    expect(Rootscope['loading']).to.equal(false);
+    expect(Rootscope['success']).to.equal(false);
+    expect(Rootscope['error']).to.equal(false);
+  });
+
+  it('should update loading to true', function () {
+    Uistates.setUIState('loading', Rootscope);
+    expect(Rootscope['initial']).to.equal(false);
+    expect(Rootscope['loading']).to.equal(true);
+    expect(Rootscope['success']).to.equal(false);
+    expect(Rootscope['error']).to.equal(false);
+  });
+
+  it('should update success to true', function () {
+    Uistates.setUIState('success', Rootscope);
+    expect(Rootscope['initial']).to.equal(false);
+    expect(Rootscope['loading']).to.equal(false);
+    expect(Rootscope['success']).to.equal(true);
+    expect(Rootscope['error']).to.equal(false);
+  });
+
+  it('should update error to true', function () {
+    Uistates.setUIState('error', Rootscope);
+    expect(Rootscope['initial']).to.equal(false);
+    expect(Rootscope['loading']).to.equal(false);
+    expect(Rootscope['success']).to.equal(false);
+    expect(Rootscope['error']).to.equal(true);
   });
 
 });
