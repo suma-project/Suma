55a7006d9d5004cea79dc29ce2cec98d78ff604a
Bret Davidson <bret_davidson@ncsu.edu>
2014-01-14 14:36:42 -0500

    update sessionsData tests

--- analysis/test/js/spec/services/sessionsData.js
+++ analysis/test/js/spec/services/sessionsData.js
@@ -18,14 +18,27 @@ describe('Service: Sessionsdata', function () {
   }));
 
   it('should make an AJAX call', function (done) {
-      $httpBackend.whenGET('lib/php/sessionsResults.php?activities=all&daygroup=all&edate=&etime=&id=1&locations=all&sdate=&session=session&session_filter=true&stime=').respond([{}, {}]);
+    $httpBackend.whenGET('lib/php/sessionsResults.php?activities=all&daygroup=all&edate=&etime=&id=1&locations=all&sdate=&session=session&session_filter=true&stime=').respond([{}, {}]);
 
-      Sessionsdata.get(params).then(function (result) {
-        expect(result.length).to.equal(2);
-        done();
-      });
-
-      $httpBackend.flush();
+    Sessionsdata.get(params).then(function (result) {
+      expect(result.length).to.equal(2);
+      done();
     });
+
+    $httpBackend.flush();
+  });
+
+  it('should respond with error message on failure', function (done) {
+    $httpBackend.whenGET('lib/php/sessionsResults.php?activities=all&daygroup=all&edate=&etime=&id=1&locations=all&sdate=&session=session&session_filter=true&stime=').respond(500, {message: 'Error'});
+
+    Sessionsdata.get(params).then(function (result) {
+
+    }, function (result) {
+      expect(result).to.deep.equal({message: 'Error', code: 500});
+      done();
+    });
+
+    $httpBackend.flush();
+  });
 
 });
