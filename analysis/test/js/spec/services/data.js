7c2c5d2ea9d6b3995a0140fa29ec0a4f895f74d1
Bret Davidson <bret_davidson@ncsu.edu>
2014-01-14 17:21:05 -0500

    update data tests

--- analysis/test/js/spec/services/data.js
+++ analysis/test/js/spec/services/data.js
@@ -9,18 +9,22 @@ describe('Service: Data', function () {
   var $httpBackend,
       $rootScope,
       okResponse,
-      params = {init: {id: 1}, count: {}, session_filter: 'false', daygroup: {}, location: {}, activity: {}},
+      params = {init: {id: 1}, count: {}, session_filter: 'false', daygroup: {}, location: {id: 'all'}, activity: {id: 'all'}},
       Processtimeseriesdata,
-      stub,
+      Processcalendardata,
+      Processhourlydata,
+      timeseriesStub,
+      calendarStub,
+      hourlyStub,
       Data;
 
-  beforeEach(inject(function (_data_, _$rootScope_, _$httpBackend_, _processTimeSeriesData_, $q) {
+  beforeEach(inject(function (_data_, _$rootScope_, _$httpBackend_, _processTimeSeriesData_, _processCalendarData_, _processHourlyData_, $q) {
     Data = _data_;
     Processtimeseriesdata = _processTimeSeriesData_;
+    Processcalendardata = _processCalendarData_;
+    Processhourlydata = _processHourlyData_;
     $rootScope = _$rootScope_;
     $httpBackend = _$httpBackend_;
-
-    stub = sinon.stub(Processtimeseriesdata, 'get');
 
     okResponse = function () {
       var dfd = $q.defer();;
@@ -29,17 +33,86 @@ describe('Service: Data', function () {
     };
   }));
 
-  it('should make an AJAX call', function (done) {
-    $httpBackend.whenGET('lib/php/dataResults.php?activities=undefined-undefined&daygroup=all&edate=&etime=&id=1&locations=all&sdate=&session=count&session_filter=false&stime=')
+  it('should make an AJAX call with processTimeSeriesData', function (done) {
+    $httpBackend.whenGET('lib/php/dataResults.php?activities=all&daygroup=all&edate=&etime=&id=1&locations=all&sdate=&session=count&session_filter=false&stime=')
       .respond([{}, {}]);
 
-    stub.returns(okResponse());
+    timeseriesStub = sinon.stub(Processtimeseriesdata, 'get');
+    timeseriesStub.returns(okResponse());
     Data.get(params, [], [], 'processTimeSeriesData').then(function (result) {
       expect(result.success).to.equal(true);
       done();
     });
 
+
+
+    // Data.get(params, [], []).then(function (result) {
+    //   console.log('result', result)
+    //   expect(result.success).to.equal(false);
+    //   done();
+    // });
+
     $httpBackend.flush();
-    stub.restore();
+    timeseriesStub.restore();
+  });
+
+  it('should make an AJAX call with processCalendarData', function (done) {
+    $httpBackend.whenGET('lib/php/dataResults.php?activities=all&daygroup=all&edate=&etime=&id=1&locations=all&sdate=&session=count&session_filter=false&stime=')
+      .respond([{}, {}]);
+
+    calendarStub = sinon.stub(Processcalendardata, 'get');
+    calendarStub.returns(okResponse());
+
+    Data.get(params, [], [], 'processCalendarData').then(function (result) {
+      expect(result.success).to.equal(true);
+      done();
+    });
+
+    $httpBackend.flush();
+    calendarStub.restore();
+  });
+
+  it('should make an AJAX call with processHourlyData', function (done) {
+    $httpBackend.whenGET('lib/php/dataResults.php?activities=all&daygroup=all&edate=&etime=&id=1&locations=all&sdate=&session=count&session_filter=false&stime=')
+      .respond([{}, {}]);
+
+    hourlyStub = sinon.stub(Processhourlydata, 'get');
+    hourlyStub.returns(okResponse());
+
+    Data.get(params, [], [], 'processHourlyData').then(function (result) {
+      expect(result.success).to.equal(true);
+      done();
+    });
+
+    $httpBackend.flush();
+    hourlyStub.restore();
+  });
+
+  it('should return an error if no processor is passed', function (done) {
+    $httpBackend.whenGET('lib/php/dataResults.php?activities=all&daygroup=all&edate=&etime=&id=1&locations=all&sdate=&session=count&session_filter=false&stime=')
+      .respond([{}, {}]);
+
+    Data.get(params, [], []).then(function (result) {
+
+    }, function(result) {
+      expect(result).to.deep.equal({message: 'Data processor not found.', code: 'None found.'})
+      done();
+    });
+
+    $httpBackend.flush();
+  });
+
+  it('should return an error if AJAX fails', function (done) {
+    $httpBackend.whenGET('lib/php/dataResults.php?activities=all&daygroup=all&edate=&etime=&id=1&locations=all&sdate=&session=count&session_filter=false&stime=')
+      .respond(500, {message: 'Error'});
+
+    Data.get(params, [], [], 'processTimeSeriesData').then(function (result) {
+
+    }, function(result) {
+      expect(result).to.deep.equal({message: 'Error', code: 500})
+      done();
+    });
+
+    $httpBackend.flush();
   });
 });
