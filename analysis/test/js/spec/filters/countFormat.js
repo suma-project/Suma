c0bdcaa992346c2a2ca1ae6738a0926b60476d3f
Bret Davidson <bret_davidson@ncsu.edu>
2014-01-23 10:51:45 -0500

    move countFormat test data to mock

--- analysis/test/js/spec/filters/countFormat.js
+++ analysis/test/js/spec/filters/countFormat.js
@@ -5,22 +5,18 @@ describe('Filter: countFormat', function () {
   // load the filter's module
   beforeEach(module('sumaAnalysis'));
 
+  beforeEach(module('countFormatMock'));
+
   // initialize a new instance of the filter before each test
-  var countFormat;
-  beforeEach(inject(function ($filter) {
+  var countFormat,
+      numbers;
+
+  beforeEach(inject(function ($filter, numbers) {
     countFormat = $filter('countFormat');
+    numbers = numbers;
   }));
 
   it('should format numbers with thousands separator:"', function () {
-    var numbers = {
-      1000: '1,000',
-      10000: '10,000',
-      100000: '100,000',
-      1000000: '1,000,000',
-      10000000: '10,000,000',
-      100000000: '100,000,000'
-    };
-
     for (var num in numbers) {
       expect(countFormat(num)).to.equal(numbers[num]);
     }
