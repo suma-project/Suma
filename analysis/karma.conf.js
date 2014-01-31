d3dcb52d2856a4e9dfddabc6882d279321225983
Bret Davidson <bret_davidson@ncsu.edu>
2014-01-31 09:33:04 -0500

    add Safari, Firefox, and Opera to karma runner

--- analysis/karma.conf.js
+++ analysis/karma.conf.js
@@ -71,7 +71,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome', 'PhantomJS'],
+    browsers: ['Chrome', 'Safari', 'PhantomJS', 'Firefox', 'Opera'],
 
 
     // Continuous Integration mode
