9314cc051abaf4a01b9ef2a2b5a8adb36bcb02a1
Bret Davidson <bret_davidson@ncsu.edu>
2014-02-11 16:08:36 -0500

    update sumaGump tests

--- analysis/test/php/spec/sumaGumpTest.php
+++ analysis/test/php/spec/sumaGumpTest.php
@@ -1,9 +1,52 @@
 <?php
 
+require_once 'src/lib/php/Gump.php';
 require_once 'src/lib/php/SumaGump.php';
 
 class SumaGumpTest extends PHPUnit_Framework_TestCase {
-  public function test_test () {
-    $this->assertEquals(5, 5);
+
+  public function testFilterRmhypen () {
+    $original = array('id' => '2014-01-01');
+    $expected = array('id' => '20140101');
+
+    $filters = array(
+      'id'         => 'rmhyphen',
+    );
+
+    $gump = new SumaGump();
+
+    $filtered = $gump->filter($original, $filters);
+
+    $this->assertEquals($expected, $filtered);
+  }
+
+  public function testFilterPadTimeEqualsThree () {
+    $original = array('id' => '300');
+    $expected = array('id' => '0300');
+
+    $filters = array(
+      'id'         => 'pad_time',
+    );
+
+    $gump = new SumaGump();
+
+    $filtered = $gump->filter($original, $filters);
+
+    $this->assertEquals($expected, $filtered);
+  }
+
+  public function testFilterPadTimeNotEqualsThree () {
+    $original = array('id' => '0300');
+    $expected = array('id' => '0300');
+
+    $filters = array(
+      'id'         => 'pad_time',
+    );
+
+    $gump = new SumaGump();
+
+    $filtered = $gump->filter($original, $filters);
+
+    $this->assertEquals($expected, $filtered);
   }
 }
