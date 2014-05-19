6907991b08fdf5d1c24640ee6f0162f7550cc856
Bret Davidson <bret_davidson@ncsu.edu>
2014-05-19 17:31:09 -0400

    prepend act group to activity title in metadata

--- analysis/reports/scripts/scripts.js
+++ analysis/reports/scripts/scripts.js
@@ -49957,12 +49957,18 @@ angular.module('sumaAnalysis').filter('truncate', function () {
 'use strict';
 angular.module('sumaAnalysis').filter('activityTitle', function () {
   return function (input, acts, mode) {
+    function getActGrpTitle(obj) {
+      return _.find(acts, {
+        id: obj.activityGroup,
+        type: 'activityGroup'
+      }).title;
+    }
     return _.map(input, function (act) {
       var obj = _.find(acts, {
           id: parseInt(act, 10),
           type: mode
         });
-      return obj ? _.unescape(obj.title) : 'None';
+      return obj ? obj.type === 'activityGroup' ? _.unescape(obj.title) : getActGrpTitle(obj) + '-' + _.unescape(obj.title) : 'None';
     }).join(', ');
   };
 });
--- analysis/src/scripts/filters/activityTitle.js
+++ analysis/src/scripts/filters/activityTitle.js
@@ -3,9 +3,13 @@
 angular.module('sumaAnalysis')
   .filter('activityTitle', function () {
     return function(input, acts, mode) {
+      function getActGrpTitle (obj) {
+        return _.find(acts, {id: obj.activityGroup, type: 'activityGroup'}).title;
+      }
+
       return  _.map(input, function (act) {
         var obj = _.find(acts, {id: parseInt(act, 10), type: mode});
-        return obj ? _.unescape(obj.title) : 'None'
+        return obj ? obj.type === 'activityGroup' ? _.unescape(obj.title) : getActGrpTitle(obj) + '-' + _.unescape(obj.title) : 'None'
       }).join(', ');
     };
   });
