9e9ca7528807aa8b0e1c7e40d1198bee3f25fb60
Bret Davidson <bret_davidson@ncsu.edu>
2014-05-20 09:16:16 -0400

    update reports build

--- analysis/reports/scripts/scripts.js
+++ analysis/reports/scripts/scripts.js
@@ -51552,10 +51552,16 @@ angular.module('sumaAnalysis').directive('sumaModal', function () {
     transclude: true,
     scope: {
       modalId: '@',
-      modalTitle: '@'
+      modalTitle: '@',
+      modalSaveText: '@'
     },
     link: function (scope, el, attrs) {
+      // Initialize modal
       $('#' + scope.modalId).modal({ show: false });
+      // Hide modal when navigating between pages
+      scope.$on('$locationChangeSuccess', function (e) {
+        $('#' + scope.modalId).modal('hide');
+      });
     }
   };
 });
