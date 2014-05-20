4b1690d499f08b8ed761bac3dfed0af7d737e397
Bret Davidson <bret_davidson@ncsu.edu>
2014-05-20 16:15:41 -0400

    update tests

--- analysis/test/js/mocks/services/dataMock.js
+++ analysis/test/js/mocks/services/dataMock.js
@@ -6,26 +6,36 @@ angular.module('dataMock', [])
   classifyCounts: '',
   wholeSession: '',
   days: '',
-  location: {id: 'all'},
-  activity: {id: 'all'}
+  zeroCounts: 'no',
+  location: {id: 'all'}
 }).value('mockParams2', {
   init: {id: 1},
   classifyCounts: {id: 'count'},
   wholeSession: {id: 'no'},
   days: ['mo', 'tu', 'we', 'th', 'fr', 'sa', 'su'],
-  location: {id: 'all'},
-  activity: {type: 'activity', id: 4}
+  location: {id: 'all'}
 }).value('mockParams3', {
   init: {id: 1},
   classifyCounts: {id: 'count'},
   wholeSession: {id: 'yes'},
   days: ['mo', 'tu', 'we', 'th', 'fr', 'sa', 'su'],
-  location: false,
-  activity: {type: 'activity', id: 4}
+  location: false
 }).value('mockParams4', {
   init: {id: 1}
-}).value('mockUrl1', 'lib/php/dataResults.php?activities=all&edate=&etime=&id=1&locations=all&sdate=&stime=')
-.value('mockUrl2', 'lib/php/dataResults.php?activities=activity-4&classifyCounts=count&days=mo,tu,we,th,fr,sa,su&edate=&etime=&id=1&locations=all&sdate=&stime=&wholeSession=no')
-.value('mockUrl3', 'lib/php/sessionsResults.php?activities=all&classifyCounts=session&days=mo,tu,we,th,fr,sa,su&edate=&etime=&id=1&locations=all&sdate=&stime=&wholeSession=yes')
-.value('mockUrl4', 'lib/php/dataResults.php?activities=activity-4&classifyCounts=count&days=mo,tu,we,th,fr,sa,su&edate=&etime=&id=1&locations=all&sdate=&stime=&wholeSession=yes')
-.value('mockUrl5', 'lib/php/sessionsResults.php?edate=&etime=&id=1&sdate=&stime=');
+}).value('mockParams5', {
+  init: {id: 1},
+  classifyCounts: '',
+  wholeSession: '',
+  days: '',
+  zeroCounts: 'no',
+  location: {id: 'all'},
+  excludeActs: [''],
+  requireActs: [''],
+  excludeActGrps: [''],
+  requireActGrps: ['']
+}).value('mockUrl1', 'lib/php/dataResults.php?edate=&etime=&id=1&locations=all&sdate=&stime=')
+.value('mockUrl2', 'lib/php/dataResults.php?classifyCounts=count&days=mo,tu,we,th,fr,sa,su&edate=&etime=&id=1&locations=all&sdate=&stime=&wholeSession=no')
+.value('mockUrl3', 'lib/php/sessionsResults.php?classifyCounts=session&days=mo,tu,we,th,fr,sa,su&edate=&etime=&id=1&locations=all&sdate=&stime=&wholeSession=yes')
+.value('mockUrl4', 'lib/php/dataResults.php?classifyCounts=count&days=mo,tu,we,th,fr,sa,su&edate=&etime=&id=1&locations=all&sdate=&stime=&wholeSession=yes')
+.value('mockUrl5', 'lib/php/sessionsResults.php?edate=&etime=&id=1&sdate=&stime=')
+.value('mockUrl6', 'lib/php/dataResults.php?edate=&etime=&excludeActGrps=&excludeActs=&id=1&locations=all&requireActGrps=&requireActs=&sdate=&stime=');
