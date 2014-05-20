860f744a638f52130c9289d35ae3c0c76a71fded
Bret Davidson <bret_davidson@ncsu.edu>
2014-05-20 16:32:44 -0400

    update tests

--- analysis/test/js/spec/services/processTimeSeriesData.js
+++ analysis/test/js/spec/services/processTimeSeriesData.js
@@ -7,67 +7,67 @@ describe('Service: Processtimeseriesdata', function () {
 
   beforeEach(module('processMock'));
 
-  // // instantiate service
-  // var Processtimeseriesdata,
-  //     $httpBackend,
-  //     MockLocations,
-  //     MockActivities,
-  //     MockResponse,
-  //     MockResponseZeros,
-  //     MockProcessedData,
-  //     MockProcessedZeros;
+  // instantiate service
+  var Processtimeseriesdata,
+      $httpBackend,
+      MockLocations,
+      MockActivities,
+      MockResponse,
+      MockResponseZeros,
+      MockProcessedData,
+      MockProcessedZeros;
 
-  // beforeEach(inject(function (
-  //   _processTimeSeriesData_,
-  //   _$httpBackend_,
-  //   mockLocations,
-  //   mockActivities,
-  //   mockResponse,
-  //   mockResponseZeros,
-  //   mockTimeSeriesProcessedData,
-  //   mockTimeSeriesProcessedZeros) {
+  beforeEach(inject(function (
+    _processTimeSeriesData_,
+    _$httpBackend_,
+    mockLocations,
+    mockActivities,
+    mockResponse,
+    mockResponseZeros,
+    mockTimeSeriesProcessedData,
+    mockTimeSeriesProcessedZeros) {
 
-  //   Processtimeseriesdata = _processTimeSeriesData_;
-  //   $httpBackend = _$httpBackend_;
-  //   MockLocations = mockLocations;
-  //   MockActivities = mockActivities;
-  //   MockResponse = mockResponse;
-  //   MockResponseZeros = mockResponseZeros;
-  //   MockProcessedData = mockTimeSeriesProcessedData;
-  //   MockProcessedZeros = mockTimeSeriesProcessedZeros;
-  // }));
+    Processtimeseriesdata = _processTimeSeriesData_;
+    $httpBackend = _$httpBackend_;
+    MockLocations = mockLocations;
+    MockActivities = mockActivities;
+    MockResponse = mockResponse;
+    MockResponseZeros = mockResponseZeros;
+    MockProcessedData = mockTimeSeriesProcessedData;
+    MockProcessedZeros = mockTimeSeriesProcessedZeros;
+  }));
 
-  // it('should build an object of data objects', function (done) {
-  //   Processtimeseriesdata.get(MockResponse, MockActivities, MockLocations, {zeroCounts: {id: 'no'}})
-  //     .then(function (data) {
-  //       expect(MockProcessedData).to.deep.equal(data);
-  //       done();
-  //     });
+  it('should build an object of data objects', function (done) {
+    Processtimeseriesdata.get(MockResponse, MockActivities, MockLocations, {zeroCounts: {id: 'no'}})
+      .then(function (data) {
+        expect(MockProcessedData).to.deep.equal(data);
+        done();
+      });
 
-  //   $httpBackend.flush();
-  // });
+    $httpBackend.flush();
+  });
 
-  // it('should take else branch if _No Activity does not exist', function (done) {
-  //   var locs = [],
-  //       acts = [],
-  //       resp = {};
+  it('should take else branch if _No Activity does not exist', function (done) {
+    var locs = [],
+        acts = [],
+        resp = {};
 
-  //   Processtimeseriesdata.get(resp, acts, locs, {zeroCounts: {id: 'yes'}})
-  //     .then(function (data) {
-  //       expect(data).to.be.an('object');
-  //       done();
-  //     });
+    Processtimeseriesdata.get(resp, acts, locs, {zeroCounts: {id: 'yes'}})
+      .then(function (data) {
+        expect(data).to.be.an('object');
+        done();
+      });
 
-  //   $httpBackend.flush();
-  // });
+    $httpBackend.flush();
+  });
 
-  // it('should handle zero/NaN conditions', function (done) {
-  //   Processtimeseriesdata.get(MockResponseZeros, MockActivities, MockLocations, {zeroCounts: {id: 'no'}})
-  //     .then(function (data) {
-  //       expect(MockProcessedZeros).to.deep.equal(data);
-  //       done();
-  //     });
+  it('should handle zero/NaN conditions', function (done) {
+    Processtimeseriesdata.get(MockResponseZeros, MockActivities, MockLocations, {zeroCounts: {id: 'no'}})
+      .then(function (data) {
+        expect(MockProcessedZeros).to.deep.equal(data);
+        done();
+      });
 
-  //   $httpBackend.flush();
-  // });
+    $httpBackend.flush();
+  });
 });
