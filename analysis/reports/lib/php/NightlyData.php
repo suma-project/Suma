b892dd05965957128b3b48f1aa2cea567e23a4c4
Ken Irwin <kirwin@wittenberg.edu>
2014-12-16 20:09:30 -0500

    remove $hourDisplay from NightlyData.php - unused

--- analysis/reports/lib/php/NightlyData.php
+++ analysis/reports/lib/php/NightlyData.php
@@ -28,37 +28,6 @@ class NightlyData
      * @access  private
      */
     private $currentInitID = "";
-    /**
-     * Hash for human-readable display of hours
-     * @var array
-     * @access  public
-     */
-    public $hourDisplay = array(
-        "0" => "12:00 AM",
-        "1" => "01:00 AM",
-        "2" => "02:00 AM",
-        "3" => "03:00 AM",
-        "4" => "04:00 AM",
-        "5" => "05:00 AM",
-        "6" => "06:00 AM",
-        "7" => "07:00 AM",
-        "8" => "08:00 AM",
-        "9" => "09:00 AM",
-        "10" => "10:00 AM",
-        "11" => "11:00 AM",
-        "12" => "12:00 PM",
-        "13" => "01:00 PM",
-        "14" => "02:00 PM",
-        "15" => "03:00 PM",
-        "16" => "04:00 PM",
-        "17" => "05:00 PM",
-        "18" => "06:00 PM",
-        "19" => "07:00 PM",
-        "20" => "08:00 PM",
-        "21" => "09:00 PM",
-        "22" => "10:00 PM",
-        "23" => "11:00 PM",
-    );
     /** 
      * Get list of active initiatives and populate $locations array
      * @return array
