b23a9b73c3773622af0b0f7024ab667bbfcdf6cd
Ken Irwin <kirwin@wittenberg.edu>
2014-12-26 19:31:02 -0500

    moved reports changes into source directory

--- analysis/src/lib/php/NightlyData.php
+++ analysis/src/lib/php/NightlyData.php
@@ -1,102 +1,114 @@
-<?php
-
-require_once 'ServerIO.php';
-
+ <?php
+ require_once 'ServerIO.php';
 /**
  * Class to create an hourly report on previous
  * day's counts for all initiatives.
  *
- * @author  Bret Davidson <bret_davidson@ncsu.edu>
- */
+ * @author Bret Davidson <bret_davidson@ncsu.edu>  */
 class NightlyData
 {
     /**
      * Placeholder for returned data.
      * @var array
-     * @access  private
+     * @access private
      */
     private $countHash = array();
     /**
-     * Hash for human-readable display of hours
+     * Placeholder for returned data.
      * @var array
-     * @access  public
+     * @access private
      */
-    public $hourDisplay = array(
-        "0" => "12:00 AM",
-        "1" => "01:00 AM",
-        "2" => "02:00 AM",
-        "3" => "03:00 AM",
-        "4" => "04:00 AM",
-        "5" => "05:00 AM",
-        "6" => "06:00 AM",
-        "7" => "07:00 AM",
-        "8" => "08:00 AM",
-        "9" => "09:00 AM",
-        "10" => "10:00 AM",
-        "11" => "11:00 AM",
-        "12" => "12:00 PM",
-        "13" => "01:00 PM",
-        "14" => "02:00 PM",
-        "15" => "03:00 PM",
-        "16" => "04:00 PM",
-        "17" => "05:00 PM",
-        "18" => "06:00 PM",
-        "19" => "07:00 PM",
-        "20" => "08:00 PM",
-        "21" => "09:00 PM",
-        "22" => "10:00 PM",
-        "23" => "11:00 PM",
-    );
+    private $locations = array();
     /**
+     * ID of initiative currently being processed
+     * @var string
+     * @access private
+     */
+    private $currentInitID = "";
+    /**
+     * Get list of active initiatives and populate $locations array
+     * @return array
+     * @access private
+     */
+    private function activeInitiatives()
+    {
+        $active = array();
+        $io     = new ServerIO();
+        $inits  = $io->getInitiatives();
+        foreach ($inits as $key => $init)
+            {
+                if ($init['enabled'] == 1)
+                    {
+                        $id          = $init['id'];
+                        $title       = $init['title'];
+                        $active[$id] = $title;
+                        foreach ($init['dictionary']['locations'] as $key => $locData)
+                            {
+                                $locID                        = $locData['id'];
+                                $locTitle                     = $locData['title'];
+                                $this->locations[$id][$locID] = $locTitle;
+                            }
+                    }
+            }
+        return $active;
+    }
+    /**
+     * Gets array of initiatives for a location
      * Builds 24 hour scaffold array for counts
      * @return array
-     * @access  private
+     * @access private
      */
-    private function buildHoursScaffold() {
-        $hours = array();
-
-        for ($i = 0; $i <= 23; $i++)
-        {
-            $hours[$i] = "n/a";
-        }
-
+    private function buildHoursScaffold()
+    {
+        $hours  = array();
+        $initID = $this->currentInitID;
+        foreach ($this->locations[$this->currentInitID] as $locID => $locTitle)
+            {
+                for ($i = 0; $i <= 23; $i++)
+                    {
+                        $hours[$i][$locID] = 0;
+                    }
+            }
         return $hours;
     }
     /**
      * Method for processing response from ServerIO
-     * @param  array $response
-     * @access  private
+     * @param array $response
+     * @access private
      */
-    private function populateHash($response) {
+    private function populateHash($response)
+    {
         // Get init title
-        $title = $response['initiative']['title'];
-
+        $title               = $response['initiative']['title'];
+        // Remember Initiative ID & Title
+        $this->currentInitID = $response['initiative']['id'];
         // Add init to COUNTHASH
         if (!isset($this->countHash[$title]))
-        {
-            $this->countHash[$title] = $this->buildHoursScaffold();
-        }
-
+            {
+                $this->countHash[$title] = $this->buildHoursScaffold();
+            }
         // Process counts
         if (isset($response['initiative']['locations']))
-        {
-            $locations = $response['initiative']['locations'];
-            foreach ($locations as $loc)
             {
-                foreach ($loc['counts'] as $count)
-                {
-                    $hour = date('G', strtotime($count['time']));
-                    if (!is_int($this->countHash[$title][$hour]))
+                $locations = $response['initiative']['locations'];
+                foreach ($locations as $loc)
                     {
-                        $this->countHash[$title][$hour] = $count['number'];
+                        $locID = $loc['id'];
+                        foreach ($loc['counts'] as $count)
+                            {
+                                $hour = date('G', strtotime($count['time']));
+                                // get a count of each location separately
+                                if (!is_int($this->countHash[$title][$hour][$locID]))
+                                    {
+                                        $this->countHash[$title][$hour][$locID] = $count['number'];
+                                    }
+                                                    else
+                                                        {
+                                                            $this->countHash[$title][$hour][$locID] += $count['number'];
+                                                        }
+                            }
                     }
-                    else
-                    {
-                        $this->countHash[$title][$hour] += $count['number'];
-                    }
-                }
             }
-        }
     }
     /**
      * Method for processing nightly data
@@ -106,57 +118,208 @@ class NightlyData
     private function processData($day)
     {
         // QueryServer config
-        $queryType = "counts";
-
+        $queryType   = "counts";
         // Create new ServerIO instance
-        $io = new ServerIO();
-
-        // Retrieve all initiatives
-        $initiatives = $io->getInitiatives();
-
-        // Build array of param arrays
-        $inits = array();
-        foreach ($initiatives as $key => $init)
-        {
-            $params = array(
-                'id'     => $init['id'],
-                'format' => "lc",
-                'sdate'  => $day,
-                'edate'  => $day
-            );
-            array_push($inits, $params);
-        }
-
+        $io          = new ServerIO();
+        // Retrieve all active initiatives
+        $initiatives = $this->activeInitiatives();
+        // Build array of param arrays for active inits
+        $inits       = array();
+        foreach ($initiatives as $id => $title)
+            {
+                $params = array(
+                                'id' => $id,
+                                'format' => "lc",
+                                'sdate' => $day,
+                'edate' => $day
+                                );
+                array_push($inits, $params);
+            }
         // Retrieve data for each initiative
         foreach ($inits as $params)
-        {
-            try
             {
-                $io = new ServerIO();
-                $this->populateHash($io->getData($params, $queryType));
-                while ($io->hasMore())
+            try
                 {
-                    $this->populateHash($io->next());
+                    $io = new ServerIO();
+                    $this->populateHash($io->getData($params, $queryType));
+                    while ($io->hasMore())
+                        {
+                            $this->populateHash($io->next());
+                        }
+                }
+            catch (Exception $e)
+                {
+                    throw $e;
                 }
             }
-            catch (Exception $e)
+    }
+    /**
+     * Build table of hourly stats by location
+     * @return string $table
+     * @access public
+     */
+    public function buildLocationStatsTable($statsArray, $initTitle)
+    {
+        $tableHeader  = array(
+            'Hour'
+                              );
+        $tableRows    = array();
+        $initID       = array_search($initTitle, $this->activeInitiatives());
+        $multipleLocs = (sizeof($this->locations[$initID]) > 1 ? true : false);
+        //build table header from locations if multiple locations
+        if ($multipleLocs)
             {
-                throw $e;
+                foreach ($this->locations[$initID] as $key => $locTitle)
+                    {
+                        array_push($tableHeader, $locTitle);
+                    }
             }
-        }
-
+        array_push($tableHeader, 'Total');
+        array_push($tableRows, $tableHeader);
+        // build table rows -- only show locations if more than one
+        foreach ($statsArray as $hour => $stats)
+            {
+                $formattedHour = date("h A", strtotime("$hour:00:00"));
+                $rowTotal      = 0;
+                $rowCells      = array(
+                $formattedHour
+                                       );
+                foreach ($stats as $locID => $count)
+                    {
+                        if (is_numeric($count))
+                            {
+                                $rowTotal += $count;
+                            }
+                        if ($multipleLocs)
+                            {
+                                array_push($rowCells, $count);
+                            }
+                    }
+                array_push($rowCells, $rowTotal);
+                array_push($tableRows, $rowCells);
+            }
+        return $tableRows;
+    }
+    /**
+     * Reduce table to just hours and totals
+     *
+     * @param array of table rows
+     * @return array newTable
+     * @access public
+     */
+    public function eliminateLocations($table)
+    {
+        $newTable = array();
+        foreach ($table as $rows)
+            {
+                $newRow = array(
+                                $rows[0],
+                                $rows[sizeof($rows) - 1]
+                                );
+                array_push($newTable, $newRow);
+            }
+        return $newTable;
+    }
+    /** 
+     * Hide from report hours with no activity
+     * @param array of table rows
+     * @return array
+     * @access public
+     */
+    public function hideZeroHours($table)
+    {
+        $newTable = array();
+        $i        = 0;
+        foreach ($table as $rows)
+            {
+                $lastIndex = sizeof($rows) - 1;
+                if ($rows[$lastIndex] != 0 || $i == 0)
+                    {
+                        array_push($newTable, $rows);
+                    }
+                $i++;
+            }
+        return $newTable;
+    }
+    /**
+     * Format table output as text or html
+     *
+     * @param array of table rows, string printFormat
+     * @return string output
+     * @access public
+     */
+    public function formatTable($rows, $printFormat = "text")
+    {
+        //get column widths
+        $columnWidth = array();
+        foreach ($rows as $row)
+            {
+                foreach ($row as $num => $val)
+                    {
+                        if (isset($columnWidth[$num]))
+                            {
+                                $columnWidth[$num] = (strlen($val) > $columnWidth[$num] ? strlen($val) : $columnWidth[$num]);
+                            }
+                else
+                    {
+                        $columnWidth[$num] = strlen($val);
+                    }
+                    }
+            }
+        $output = "";
+        foreach ($rows as $row)
+            {
+                $format = '%-' . $columnWidth[0] . 's';
+                for ($i = 1; $i < sizeof($row); $i++)
+                    {
+                        $format .= ' %' . $columnWidth[$i] . 's';
+                    }
+                if ($printFormat == "html")
+                    {
+                        $output .= "<tr><td>" . join("</td>\n<td>", $row) . "</td></tr>" . PHP_EOL;
+                    }
+            else
+                {
+                    $output .= vsprintf($format, $row) . "\n";
+                }
+            }
+        if ($printFormat == "html")
+            {
+                $output = "<table>$output</table>\n";
+            }
+        return $output;
+    }
+    /**
+     * Turn array data sideways (rows <-> columns)
+     *
+     * @param array $rows
+     * @return array
+     * @access public
+     */
+    public function sideways($rows)
+    {
+        $sideways = array();
+        $i        = 0;
+        foreach ($rows as $array)
+            {
+                foreach ($array as $k => $v)
+                    {
+                        $sideways[$k][$i] = $v;
+                    }
+                $i++;
+            }
+        return $sideways;
     }
     /**
      * Get data from server
      *
-     * @param  string $day YYYYMMDD string for date
+     * @param string $day YYYYMMDD string for date
      * @return array
-     * @access  public
+     * @access public
      */
     public function getData($day)
     {
         $this->processData($day);
-
         return $this->countHash;
     }
 }
--- analysis/src/lib/php/nightly.php
+++ analysis/src/lib/php/nightly.php
@@ -1,42 +1,83 @@
 <?php
-
 require_once 'vendor/autoload.php';
 require_once 'NightlyData.php';
+
+/* get command-line arguments if present. allowed values:
+   locations
+   --hours-across
+   --html
+   --hide-zeros
+*/
+$locationBreakdown = (array_search("locations", $argv) ? (array_search("locations", $argv) > 0 ? true : "") : false);
+$hoursAcross = (array_search("--hours-across", $argv) ? (array_search("--hours-across", $argv) > 0 ? true : "") : false);
+$outputHtml = (array_search("--html", $argv) ? (array_search("--html", $argv) > 0 ? true : "") : false);
+$hideZeroHours = (array_search("--hide-zeros", $argv) ? (array_search("--hide-zeros", $argv) > 0 ? true : "") : false);
 
 // Configuration
 $config = Spyc::YAMLLoad(realpath(dirname(__FILE__)) . '/../../../config/config.yaml');
 
 if (isset($config['nightly']))
-{
-    // Default Timezone. See: http://us3.php.net/manual/en/timezones.php
-    $DEFAULT_TIMEZONE = $config['nightly']['timezone'];
-    date_default_timezone_set($DEFAULT_TIMEZONE);
-
-    // Which day to retrieve hourly report
-    $DAY_PROCESS = date('Ymd', strtotime('yesterday'));
-
-    // Initialize class and retrieve data
-    try
     {
-        $data = new NightlyData();
-        $nightlyData = $data->getData($DAY_PROCESS);
-
-        // Print Output
-        foreach ($nightlyData as $key => $init)
+        // Default Timezone. See: http://us3.php.net/manual/en/timezones.php
+        $DEFAULT_TIMEZONE = $config['nightly']['timezone'];
+        date_default_timezone_set($DEFAULT_TIMEZONE);
+    
+        // Which day to retrieve hourly report
+        $DAY_PROCESS = date('Ymd', strtotime('yesterday'));
+    
+        // Initialize class and retrieve data
+    try
         {
-            print "\n" . $key . "\n";
-            foreach ($init as $key => $count)
-            {
-                print " " . $data->hourDisplay[$key] . ': ' . $count . "\n";
-            }
+            $data        = new NightlyData();
+            $nightlyData = $data->getData($DAY_PROCESS);
+        
+            // Print Output
+        
+            // Add stylesheet if multiple locations
+            if ($outputHtml)
+                {
+                    print '<style>';
+                    print 'table { border-collapse: collapse;}';
+                    print 'td,th { border: 1px solid black; text-align: center;}';
+                    print 'tr:first-child { font-weight: bold }';
+                    print 'td:first-child { font-weight: bold }';
+                    print '</style>';
+                }
+        
+            foreach ($nightlyData as $key => $init)
+                {
+                    $table = ($data->buildLocationStatsTable($nightlyData[$key], $key));
+            
+                    if (!$locationBreakdown)
+                        {
+                            $table = $data->eliminateLocations($table);
+                        }
+                    if ($hideZeroHours)
+                        {
+                            $table = $data->hideZeroHours($table);
+                        }
+                    if ($hoursAcross)
+                        {
+                            $table = $data->sideways($table);
+                        }
+                    if ($outputHtml)
+                        {
+                            print "<h2>" . $key . "</h2>\n";
+                            print($data->formatTable($table, "html"));
+                        }
+            else
+                {
+                    print "\n" . $key . "\n";
+                    print($data->formatTable($table));
+                }
+                }
+        }
+    catch (Exception $e)
+        {
+            print "Error: " . $e;
         }
     }
-    catch (Exception $e)
-    {
-        print "Error: " . $e;
-    }
-}
 else
-{
-    print 'Error: Problem loading config.yaml. Please verify config.yaml exists and contains a valid baseUrl.';
-}
+    {
+        print 'Error: Problem loading config.yaml. Please verify config.yaml exists and contains a valid baseUrl.';
+    }
