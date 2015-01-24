6cec3bb2aececc1702ff196b4f13fc8b58816773
Michael Bond <mbond@the-forgotten.org>
2015-01-24 08:14:09 -0500

    Added 403.php
    Added a 403 page so that system administrators have the option of setting 403 (forbidden) to a 404 or leaving it the default 403.
    
    By setting the 403 forbidden error to a 404 not found error we are effectively hiding any directories that someone does not have access to view. This will prevent automated security auditing software, such as IBM's AppScan, to flag a security warning for hidden directories.

---
+++ web/errors/403.php
@@ -0,0 +1,18 @@
+<?php
+
+header("HTTP/1.0 403 Forbidden");
+
+?>
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>403 - Forbidden</title>
+</head>
+<body>
+<h1>403 - Forbidden</h1>
+
+<p>We are sorry, but you do not have access to this resource.</p>
+
+</body>
+</html>
