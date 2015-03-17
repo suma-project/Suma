07412ef317b37f98ef3e8f53329123934f041e0c
Bret Davidson <bret_davidson@ncsu.edu>
2015-03-17 14:56:52 -0400

    test analysis tools with phantomjs only

--- analysis/karma.conf.js
+++ analysis/karma.conf.js
@@ -73,7 +73,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome', 'Firefox', 'PhantomJS'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
