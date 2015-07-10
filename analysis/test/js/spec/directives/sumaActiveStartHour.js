5e78db5024a6ae8b103c4b8aed2edb53f50206d5
Bret Davidson <bret_davidson@ncsu.edu>
2015-07-10 08:43:05 -0400

    test sumaActiveStartHou directive

---
+++ analysis/test/js/spec/directives/sumaActiveStartHour.js
@@ -0,0 +1,39 @@
+'use strict';
+
+describe('Directive: sumaActiveStartHour', function () {
+
+  // load the directive's module
+  beforeEach(module('sumaAnalysis'));
+
+  beforeEach(module('views/directives/activeStartHour.html'));
+
+  var element,
+    linkScope,
+    scope;
+
+  beforeEach(inject(function ($rootScope, $compile) {
+    element = angular.element('<div suma-active-start-hour start-hour="startHour"></div>');
+
+    scope = $rootScope.$new();
+    $compile(element)(scope);
+    scope.$digest();
+
+    linkScope = element.isolateScope();
+  }));
+
+  it(':sumaActiveStartHour should set scope.display to false if startHour is 0000', function () {
+    scope.$apply(function() {
+      scope.startHour = {id: '0000'};
+    });
+
+    expect(linkScope.display).to.equal(false);
+  });
+
+  it(':sumaActiveFilters should set scope.display to true if startHour is not 0000', function () {
+    scope.$apply(function() {
+      scope.startHour = {id: '0100'};
+    });
+
+    expect(linkScope.display).to.equal(true);
+  });
+});
